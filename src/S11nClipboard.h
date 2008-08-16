464284fe2f952bf45724d78183e2bbb169d55f87
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-08-16 13:16:59 +0000

    Added some docs and additional error checking.

--- src/S11nClipboard.h
+++ src/S11nClipboard.h
@@ -18,14 +18,9 @@
 #include <QDebug>
 
 /**
-   S11nClipboard provides clipboard features for any
-   Serializable type.
-
-   Bugs:
-
-   - Each instance updates the QApplication::clipboard()
-   independently, but does not look for "incoming" changes
-   made by other instances.
+   S11nClipboard provides clipboard features for any Serializable
+   type. It is intended to be used as a Singleton - fetch its instance
+   via the static instance() function.
 */
 class S11nClipboard : public QObject
 {
@@ -36,66 +31,95 @@ private:
 public:
     typedef s11nlite::node_type S11nNode;
     typedef s11nlite::node_traits S11nNodeTraits;
+    /**
+       Returns the shared instance of S11nClipboard.
+    */
     static S11nClipboard & instance();
 
     /**
        Returns the clipboard contents, which may be 0.
        The returned object is owned by this object.
+
+       Any change to the clipboard may invalidate this pointer, so do
+       not keep a reference to it.
     */
     S11nNode * contents();
 
     /** 
-	Transfers ownership of the clipboard contents
-	(which may be 0) to the caller. Before the transfer,
-	signalUpdated() is fired if the content is not 0.
+	Transfers ownership of the clipboard contents (which may be 0)
+	to the caller. If the content is non-zero then the Qt
+	clipboard is also cleared and signalUpdated() is fired.
     */
     S11nNode * take();
 
-
     /**
-       Tries to deserialize the contents as the given type.  The
-       caller owns the returned object.
+       Tries to deserialize the current clipboard contents as the
+       given type. The caller owns the returned object, which may be
+       0.
     */
     template <typename SerializableType>
     SerializableType * deserialize()
     {
-	return m_node
-	    ? s11nlite::deserialize<SerializableType>( *m_node )
-	    : 0;
+	try
+	{
+	    return m_node
+		? s11nlite::deserialize<SerializableType>( *m_node )
+		: 0;
+	}
+	catch(...)
+	{
+	    return 0;
+	}
     }
     /**
        Similar to slotCopy(), but takes a Serializable instead of
        an S11nNode. If serialization of ser fails then the clipboard
        is not updated.
+
+       If this call succeeds, the current content of the clipboard
+       will be a serialized copy of ser.
     */
     template <typename SerializableType>
     bool serialize( SerializableType & ser )
     {
-	S11nNode tmp;
-	bool ret = s11nlite::serialize( tmp, ser );
-	if( ret )
+	bool ret = false;
+	try
 	{
-	    if( ! m_node ) m_node = S11nNodeTraits::create("clipboard");
-	    S11nNodeTraits::swap( *m_node, tmp );
-	    this->syncToQt();
+	    S11nNode tmp;
+	    ret = s11nlite::serialize( tmp, ser );
+	    if( ret )
+	    {
+		if( ! m_node ) m_node = S11nNodeTraits::create("clipboard");
+		S11nNodeTraits::swap( *m_node, tmp );
+		this->syncToQt();
+	    }
+	    else
+	    {
+		qDebug() << "S11nClipboard::serialize() failed! Serializable class name is"
+			 << s11n::s11n_traits<SerializableType>::class_name(&ser).c_str();
+	    }
 	}
-	else
+	catch(...)
 	{
-	    qDebug() << "S11nClipboard::serialize() failed! Serializable class name is"
-		     << s11n::s11n_traits<SerializableType>::class_name(&ser).c_str();
+	    ret = false;
 	}
 	return ret;
     }
 public Q_SLOTS:
-   /* transfers ownership of 'take' to this object. */
+   /* Transfers ownership of 'take' to this object and sets it as the
+      current clipboard content. */
    void slotCut( S11nNode * take );
-    /* makes a copy of the given node. */
+    /* Sets the current clipboard content to a copy of the given
+       node. If passed 0 then the clipboard is cleared.
+    */
     void slotCopy( S11nNode const * copy );
+
     /* Clears the clipboard contents. */
     void slotClear();
 Q_SIGNALS:
     /** Tells listeners that this object's clipboard state has
-	(likely) been updated.*/
+	(likely) been updated. Use content() to get the clipboard data.
+    */
     void signalUpdated();
 private Q_SLOTS:
     /** Copies the local s11n clipboard data to the Qt clipboard as text and emits signalUpdated(). */
