234343a74d77b0b25912f0191d9264a3debf6cb6
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-08-16 14:30:25 +0000

    Fixes and cleanups to the list/map s11n.

--- trunk/src/reg_qt_s11n.h
+++ trunk/src/reg_qt_s11n.h
@@ -19,7 +19,7 @@ struct S11N_SERIALIZE_FUNCTOR
 	/** Serializes src to dest. */
 	bool operator()( S11nNode & dest, S11N_TYPE const & src ) const;
 	/** Deserializes dest from src. */
-	bool operator()( S11nNode const & src, S11N_TYPE & dest );
+	bool operator()( S11nNode const & src, S11N_TYPE & dest ) const;
 };
 #include <s11n.net/s11n/reg_s11n_traits.hpp>
 
