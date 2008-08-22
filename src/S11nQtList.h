b0d8bd59579e35a0646408ac71f63a87fa2657d4
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-08-22 19:34:21 +0000

    Added de/serializeQGIList().

--- src/S11nQtList.h
+++ src/S11nQtList.h
@@ -24,14 +24,11 @@
 #include <memory>
 #include <iterator> // insert_iterator
 #include <algorithm> // for_each()
+
+#include <QGraphicsItem>
+
 #include "S11nQt.h"
-#include <s11n.net/s11n/variant.hpp> // lexical casting
-#include <s11n.net/s11n/traits.hpp> // node_traits<>
-#include <s11n.net/s11n/type_traits.hpp> // type_traits<>
-#include <s11n.net/s11n/serialize.hpp> // core serialize funcs
 #include <s11n.net/s11n/abstract_creator.hpp> // abstract_creator class
-#include <s11n.net/s11n/exception.hpp> // s11n_exception class
-#include <s11n.net/s11n/s11n_debuggering_macros.hpp> // tracing macros
 #include <s11n.net/s11n/proxy/listish.hpp>
 
 namespace s11n { namespace qt {
@@ -138,6 +135,74 @@ namespace s11n { namespace qt {
 			return deserialize_qlist<NodeType,SerType>( src, dest );
 		}
 	};
+
+
+    /**
+       For each item in src, if it can be cast to a (SerializableT*)
+       then it is serialized, otherwise it is skipped.
+
+       Returns the number of objects serialized, or -1 if
+       serialization fails. It may propogate an exception on error.
+    */
+    template <typename SerializableT>
+    int serializeQGIList( S11nNode & dest,
+			  QList<QGraphicsItem *> const & src,
+			  bool skipParentedItems )
+    {
+	typedef QList<QGraphicsItem *> QGIL;
+	typedef QList<SerializableT *> QSL;
+	QSL serlist;
+	for( QGIL::const_iterator it = src.begin();
+	     src.end() != it; ++it )
+	{
+	    if( skipParentedItems && (*it)->parentItem() ) continue;
+	    SerializableT * ser = dynamic_cast<SerializableT *>(*it);
+	    if( ! ser ) continue;
+	    serlist.push_back(ser);
+	}
+	return s11n::serialize( dest, serlist )
+	    ? serlist.size()
+	    : -1;
+    }
+
+    /**
+       The converse of serializeQGIList(), it extracts
+       (SerializableT*) objects from src and tries to cast
+       them to QGraphicsItems. Dest is populated with all
+       items for which the conversion succeeds.
+
+       Returns the number of deserialized items, or -1 if
+       deserialization fails. It may propogate an exception on error.
+    */
+    template <typename SerializableT>
+    int deserializeQGIList( S11nNode const & src,
+			    QList<QGraphicsItem*> & dest,
+			    QGraphicsItem * parent = 0,
+			    QGraphicsItem::GraphicsItemFlags flags = 0 )
+    {
+	typedef QList<QGraphicsItem*> QGIL;
+	typedef QList<SerializableT *> QSL;
+	QSL serlist;
+	if( ! s11n::deserialize( src, serlist ) ) return -1;
+	int ret = 0;
+	for( typename QSL::iterator it = serlist.begin();
+	     serlist.end() != it; ++it )
+	{
+	    QGraphicsItem * qgi = dynamic_cast<QGraphicsItem *>(*it);
+	    if( ! qgi )
+	    {
+		s11n::cleanup_serializable( (*it) );
+		continue;
+	    }
+	    if( parent ) qgi->setParentItem( parent );
+	    if( flags ) qgi->setFlags( flags );
+	    dest.push_back(qgi);
+	    ++ret;
+	}
+	return ret;
+    }
+
+
 }} // namespaces
 
 
