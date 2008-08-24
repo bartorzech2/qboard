0a4b0a5e81044ca7f1bc8222825593babd483a64
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-08-24 16:58:20 +0000

    egg

---
+++ trunk/makeAltBuild.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+if [[ "$1x" = "x" ]]; then
+    echo "Usage: $0 QT_VERSION"
+    exit 1
+fi
+
+DEST=alt-$1
+
+test -d $DEST && rm -fr $DEST
+mkdir ${DEST}
+test -d ${DEST} || {
+    echo "Destination dir [${DEST}] could not be created!"
+    exit 1
+}
+
+sdirs="apps counters resources src s11n ui plugins"
+echo "Copying files..."
+for d in $sdirs; do
+    echo -e "\t... $d"
+    find $d \
+	-name '*.h' \
+	-o -name '*.?pp' \
+	-o -name '*.pro' \
+	-o -name '*.ui' \
+	-o -name '*.qrc' \
+	-o -name '*.png' \
+	-o -name '*.html' \
+	| sed -e '/\/bak/d' \
+	-e '/\/nono/d' \
+	-e '/\/release/d' \
+	-e '/\/debug/d' \
+	-e '/\.#/d' \
+	-e '/\.svn/d' \
+	-e '/qrc_*.cpp/d' \
+	> filelist
+    cp --parents $(cat filelist) $DEST || {
+	err=$?
+	echo "Copy failed!"
+	rm filelist
+	exit $err
+    }
+    rm filelist
+done
+
+cd $DEST && {
+    orig=$PWD/..
+
+    for i in config.qmake QBoard.pro; do
+	ln -s ../$i .
+    done
+    dirs=$(find . -type d -name '[a-zA-Z1-9]*')
+    echo dirs=${dirs}
+    for d in $dirs; do
+	cd $d
+	echo "==> $PWD"
+	for f in *; do
+	test -L $f && continue
+	test -d $f && continue
+	rm $f
+	ln -s $orig/$d/$f .
+	echo -e "\tLinked $f"
+	done
+	echo "<== $PWD"
+	cd -
+    done
+}
+cd -
