0651bfed29a3af3e9d30751b755052d431eae296
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-08-24 13:35:03 +0000

    Added QBoardScene class.

---
+++ src/QBoardScene.h
@@ -0,0 +1,42 @@
+#ifndef QBOARD_QBoardScene_H_INCLUDED
+#define QBOARD_QBoardScene_H_INCLUDED 1
+
+#include <QObject>
+#include <QGraphicsScene>
+class QPainter;
+class QWidget;
+#include <QGraphicsItem>
+#include <QStyleOptionGraphicsItem>
+
+#include "Serializable.h"
+
+class QBoardScene : public QGraphicsScene,
+		    public Serializable
+{
+Q_OBJECT
+public:
+    QBoardScene();
+    virtual ~QBoardScene();
+
+    /**
+       Serializes this object to dest.
+    */
+    virtual bool serialize( S11nNode & dest ) const;
+
+    /**
+       Deserializes this object from src.
+    */
+    virtual bool deserialize( S11nNode const & src );
+
+protected:
+    virtual void drawItems( QPainter * painter,
+			    int numItems,
+			    QGraphicsItem ** items,
+			    const QStyleOptionGraphicsItem * options,
+			    QWidget * widget = 0 );
+private:
+    struct Impl;
+    Impl * impl;
+};
+
+#endif // QBOARD_QBoardScene_H_INCLUDED
