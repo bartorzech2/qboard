301c92e1828ae4e878618c8f1a36db35969299ca
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-08-26 13:23:25 +0000

    fixed: custom labels were ignored.

--- resources/javascript/bogodice.jquery.pack.js
+++ resources/javascript/bogodice.jquery.pack.js
@@ -1,1 +1,163 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('g.t.x=f(2){2=g.m({a:6,3:1,e:1,q:k,j:w},2?2:[]);2=g.m({s:2.3+\'d\'+2.a},2);f o(e,a,3,5){7(!3)3=1;b 4=5?y u():k;b 8=0;b c=0;v(b i=0;i<3;++i){c=l.D(l.C()*a+e);8+=c;7(4)4[i]=c}7(5){b 9=\'\'+8;7(2.j){9=3+\'d\'+a+\': \';7(3==1){9+=8}r{9+=(4.z(\' + \'))+\' = \'+8}}7(\'f\'==A 5){5(9,4)}r{$(5).p(9,4)}}n 8};h.p(2.s);h.B(f(){o(2.e,2.a,2.3,2.q)});n h};',40,40,'||props|count|accum|target||if|total|msg|sides|var|res||low|function|jQuery|this||verbose|null|Math|extend|return|rollDie|text|resultsTarget|else|label|fn|Array|for|true|bogoDice|new|join|typeof|click|random|floor'.split('|'),0,{}))
+/**
+
+This plugin provides basic "dice rolling" features, intended
+for use in games/gaming utilities based on jQuery.
+
+Usage:
+
+	<button id='MyDieButton'></button>
+
+	$('#MyDieButton').bogoDice({ ... options ... });
+
+Results are undefined if this plugin is applied to a non-button
+UI element, but in theory it "should" work with any element
+for which jQuery(...).click() behaves properly.
+
+Options are:
+
+	- sides [integer, default=6], the number of sides the dice have.
+	Results are undefined if this is not a positive integer.
+
+	- count [integer, default=1], the number of dice to roll.
+
+	- low [integer, default=1], the lowest value of the die. This is
+	only for special-case use, like a die which should roll -1..+1
+	(e.g. Fudge dice). The sides parameter is used to determine the
+	high value.
+
+	- resultsTarget [Mixed, default=null], a target object to send the
+	dice results to. If it is a function, it is called and passed
+	the total of a dice roll result (as a string) and an array containing
+	each individual roll. If it is not a function then it is assumed to
+	be a jQuery selector, and $(resultsTarget).text(...) will be called
+	and passed the die results.
+
+	- verbose [Boolean, default=true], if false then only the
+	grand total of a die result is sent to the resultsTarget,
+	otherwise an "informative" form is used. e.g. rolling 3d6
+	will have a result in the form of "3 + 2 + 5 = 10".
+
+	- label [string, default=count+'d'+sides], a label string to
+	apply to the target button.
+
+///////////////////////////////////////////////////////////////////////
+Example usage:
+
+	<span id='DieResults'>Click a die button</span>
+	<button id='Button1d6'></button>
+	<button id='Button5d6'></button>
+...
+	$('#Button1d6').bogoDice({
+		resultsTarget:'#DieResults'
+	});
+
+	$('#Button5d6').bogoDice({
+		count:5,
+		label:'5 dice',
+		resultsTarget:'#DieResults'
+	});
+
+
+	We can support Fudge (www.fudgerpg.com) dice like this:
+
+	<button id='Button4dF'></button>
+
+	$('#Button4dF').bogoDice({
+		count:4,
+		sides:3,
+		low:-1,
+		label:'4dF',
+		verbose:false,
+		resultsTarget:function(total,accum){
+			$('#DiceResults').text('4dF ['+accum.join(', ')+"] = "+total);
+		}
+	});
+///////////////////////////////////////////////////////////////////////
+Potential TODOs:
+
+- Ability to fetch results using Ajax, to allow server-produced
+results.
+
+- Add a modifier to the roll total (e.g. 3d6+3 or 1d6-1).
+
+- Somehow support special types of dice, like dF (Fudge dice).
+
+///////////////////////////////////////////////////////////////////////
+Author:
+
+	http://wanderinghorse.net/home/stephan/
+
+Plugin home page:
+
+	http://wanderinghorse.net/computing/javascript/jquery/bogodice/
+
+License: Public Domain
+
+
+Revision history:
+
+- 20080825:
+        - Added "Roll #xyz" prefix to the output, to avoid confusion when two
+	successive rolls have the same result.
+
+- 20070815:
+	- added .low=integer option to support Fudge dice.
+	- resultsTarget callback function now passed array of results.
+	- Corrected completely bogus examples.
+	- Changed .count default from 3 to 1.
+
+- 20070805: initial release
+
+
+*/
+jQuery.fn.bogoDice = function( props ) {
+	props = jQuery.extend({
+		sides:6,
+		count:1,
+		low:1,
+		resultsTarget:null,
+		verbose:true,
+		rollCount:0,
+		},
+		props ? props : []);
+	props = jQuery.extend({
+		label:props.count+'d'+props.sides
+		},
+		props);
+
+	function rollDie(low,sides,count,target) {
+		if( ! count ) count = 1;
+		++props.rollCount;
+		var accum = target ? new Array() : null;
+		var total = 0;
+		var res =0;
+		for( var i = 0; i < count; ++i ) {
+			res = Math.floor(Math.random()*sides+low);
+			total += res;
+			if( accum ) accum[i] = res;
+		}
+		if( target ) {
+		    var prefix = 'Roll #'+props.rollCount+': ';
+			var msg = prefix+total;
+			if( props.verbose ) {
+				msg = prefix + props.label+': ';
+				if( count == 1 ) { msg += total; }
+				else {
+            		msg += (accum.join(' + '))+' = '+total;
+				}
+			}
+			if( 'function' == typeof target ) {
+				target( msg,accum );
+			} else {
+				$(target).text( msg,accum );
+			}
+		}
+		return total;
+	};
+
+	this.text(props.label);
+	this.click(function(){
+			rollDie( props.low, props.sides, props.count, props.resultsTarget );
+		});
+	return this;
+};
