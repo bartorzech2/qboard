2681b4f08c438e04ae626abf4e68f5627793d33c
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-08-27 13:59:55 +0000

    Refactored some generic utility code from GameState.* to ScriptQt.*.

--- trunk/src/ScriptQt.h
+++ trunk/src/ScriptQt.h
@@ -1,5 +1,17 @@
 #ifndef QBOARD_ScriptQt_H_INCLUDED
 #define QBOARD_ScriptQt_H_INCLUDED 1
+/*
+ * This file is (or was, at some point) part of the QBoard project
+ * (http://code.google.com/p/qboard)
+ *
+ * Copyright (c) 2008 Stephan Beal (http://wanderinghorse.net/home/stephan/)
+ *
+ * This file may be used under the terms of the GNU General Public
+ * License versions 2.0 or 3.0 as published by the Free Software
+ * Foundation and appearing in the files LICENSE.GPL2 and LICENSE.GPL3
+ * included in the packaging of this file.
+ *
+ */
 
 #include <QObject>
 #include "Serializable.h"
