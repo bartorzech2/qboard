aab541a608bdfc6cf5a735ddb02ffd34ef0fc212
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-09-01 21:21:03 +0000

    added very basic JS editor plugin

---
+++ plugins/JSEditor/JSEditor.h
@@ -0,0 +1,26 @@
+#ifndef CGMEJOE_H_INCLUDED
+#define CGMEJOE_H_INCLUDED 1
+
+#include <QObject>
+#include "QBoardPlugin.h"
+class JSEditor : public QObject,
+		public QBoardPlugin
+    
+{
+Q_OBJECT
+Q_INTERFACES(QBoardPlugin)
+public:
+    JSEditor();
+    virtual ~JSEditor();
+    virtual QWidget * widget();
+    virtual void setGameState(GameState &);
+
+private Q_SLOTS:
+    void widgetDestroyed();
+    void evalJS();
+private:
+    struct Impl;
+    Impl * impl;
+};
+
+#endif // CGMEJOE_H_INCLUDED
