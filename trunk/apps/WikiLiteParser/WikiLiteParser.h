7739a918b435443a87cc959d37d9a61adee5302b
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-09-04 00:29:02 +0000

    Significant refactoring. Got the line spacing working more or less how'd i like it to. Added CSS class WLP to all generated HTML tags.

--- trunk/apps/WikiLiteParser/WikiLiteParser.h
+++ trunk/apps/WikiLiteParser/WikiLiteParser.h
@@ -21,6 +21,68 @@
 namespace qboard {
     using namespace Ps;
 
+    /**
+       This class implements a basic Wiki-like parser, the goal
+       being to emulate Google Code's wiki fairly closely.
+
+       Currently working wiki features:
+
+       *bold*
+
+       _italics_
+
+       =Header1=
+       ...
+       =====Header5=====
+
+       `fixed width font`
+
+       ^Superscript^
+
+       ,,Subscript,,
+
+       ~~Strikethrough~~
+
+       ---- (horizontal rule)
+
+       Numered and unnumbered lists, with consecutive lines, each
+       starting with at least two spaces followed by a single '*'
+       (unnumbered) or '#' (numbered).  Nested lists are not supported
+       because figuring out when we need to close a level is a pain in
+       the ass.
+
+       {{{
+       verbatim (code) blocks (PRE tags)
+       }}}
+
+
+       All HTML tags creates by this class have a CSS class of 'WLP'
+       (as in Wiki-Lite-Parser), so one may customize their appearance
+       by configuring that class via CSS.
+
+
+       NOTABLE TODOs:
+
+       || Tables ||
+
+       [WikiLinks with optional label text]
+
+       [link_to_image.{jpg,png} optional label text]
+
+       There are no plans to support non-bracketed WikiWords.
+
+
+       Other notes:
+
+       Keep in mind that wiki parsing is fragile by nature, so don't
+       expect this to gracefully handle wildly invalid input.
+
+       The line spacing could be handled differently. Right now we use
+       BR tags, but we should wrap the paragraphs in P tags. Knowing
+       exactly when to open and close them is problematic here,
+       though.
+
+    */
     class WikiLiteParser
     {
     public:
@@ -29,8 +91,22 @@ namespace qboard {
 	~WikiLiteParser()
 	{}
 
-	bool parse( QString const & code,
+	/**
+	   Parses the wiki-formated input text and sends the output
+	   (HTML-format) to the given output device.
+
+	   If the parsing engine throws an exception, this routine will
+	   stop parsing and will append the error string to the output,
+	   then return false.
+	*/
+	bool parse( QString const & wiki,
 		    QIODevice * out );
+	/**
+	   Parses the wiki-formated input text and returns the result
+	   (HTML format) as a string.
+	*/
+	QString parse( QString const & code );
+
     };
 
 }
