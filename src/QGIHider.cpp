3e6c6c37964cd5960e25374a37a6e1f08e871636
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-09-10 12:42:43 +0000

    Added color menus. Changed how it internally stores some of its data.

--- src/QGIHider.cpp
+++ src/QGIHider.cpp
@@ -13,14 +13,29 @@
 #include <qboard/utility.h>
 #include <qboard/MenuHandlerGeneric.h>
 #include <qboard/S11nQt/QPointF.h>
+#include <qboard/S11nQt/QBrush.h>
+#include <qboard/S11nQt/QTransform.h>
 
 struct QGIHider::Impl
 {
     QGraphicsItem * item;
-    Qt::BrushStyle bstyle;
+    QPen pen;
+    //QBrush brush;
+    enum PropIDs {
+    PropUnknown = 0,
+    PropAlpha,
+    PropAngle,
+    PropColor,
+    PropDragDisabled,
+    PropPos,
+    PropRadius,
+    PropScale,
+    PropZLevel,
+    PropEND
+    };
     Impl()
 	: item(0),
-	  bstyle(Qt::SolidPattern)
+	  pen()
     {
     }
     ~Impl()
@@ -39,13 +54,123 @@ QGIHider::QGIHider() : QObject(),
 
 void QGIHider::setup()
 {
-    this->setBrush( QBrush(QColor(Qt::lightGray), impl->bstyle) );
+    this->setBrush( QBrush(QColor(Qt::lightGray),Qt::SolidPattern) );
+
 }
 
 QGIHider::~QGIHider()
 {
     //qDebug() << "QGIHider::~QGIHider()";
     delete impl;
+}
+void QGIHider::refreshTransformation()
+{
+    QVariant v( this->property("angle") );
+    qreal angle = v.canConvert<qreal>() ? v.value<qreal>() : 0.0;
+    v = this->property("scale");
+    qreal scale = v.canConvert<qreal>() ? v.value<qreal>() : 1.0;
+    if( 0 == scale ) scale = 1.0;
+    QTransform tr( qboard::rotateAndScale( this->boundingRect(), angle, scale, scale, true ) );
+    this->setTransform( tr );
+}
+
+
+void QGIHider::propertySet( char const *pname, QVariant const & var )
+{
+    typedef QMap<QString,int> PMAP;
+    static PMAP pmap;
+    if( pmap.isEmpty() )
+    {
+#define MAP(S,V) pmap[S] = Impl::V;
+	MAP("color",PropColor);
+	MAP("alpha",PropAlpha);
+	MAP("zLevel",PropZLevel);
+	MAP("pos",PropPos);
+	//MAP("scale",PropScale);
+	//MAP("angle",PropAngle);
+	//MAP("dragDisabled",PropDragDisabled);
+#undef MAP
+    }
+    int kid = pmap.value( QString(pname?pname:""), Impl::PropUnknown );
+    if( Impl::PropUnknown == kid ) return;
+
+    // FIXME: treat QVariant::Invalid appropriately for each property
+    if(0) qDebug() << "QGIDot::propertySet("<<pname<<") val="<<var;
+    if( Impl::PropZLevel == kid )
+    {
+	this->setZValue(var.toDouble());
+	this->update();
+	return;
+    }
+    if( Impl::PropPos == kid )
+    {
+	this->setPos( var.value<QPointF>() );
+	this->update();
+	return;
+    }
+    if( (Impl::PropScale == kid) || (Impl::PropAngle == kid) )
+    {
+	this->refreshTransformation();
+	return;
+    }
+    if( Impl::PropColor == kid )
+    {
+	QBrush br( this->brush() );
+	QColor old( br.color() );
+	QColor col = var.value<QColor>();
+	if( 255 == col.alpha() )
+	{
+	    col.setAlpha(old.alpha());
+	}
+	br.setColor(col);
+	this->setBrush( br );
+	this->update();
+	return;
+    }
+    if( Impl::PropAlpha == kid )
+    {
+	qreal a = var.toDouble();
+	QBrush br = this->brush();
+	QColor col = br.color();
+	if( a > 1 )
+	{ // assume it's int-encoded
+	    col.setAlpha( int(a) );
+	}
+	else
+	{
+	    col.setAlphaF(a);
+	}
+	br.setColor(col);
+	this->setBrush( br );
+    	this->update();
+	return;
+    }
+    if( Impl::PropDragDisabled == kid )
+    {
+	if( var.isValid() )
+	{
+	    this->setFlag( QGraphicsItem::ItemIsMovable, var.toInt() ? false : true );
+	}
+	else
+	{
+	    this->setFlag( QGraphicsItem::ItemIsMovable, true );
+	}
+	return;
+    }
+}
+
+bool QGIHider::event( QEvent * e )
+{
+    while( e->type() == QEvent::DynamicPropertyChange )
+    {
+	QDynamicPropertyChangeEvent *chev = dynamic_cast<QDynamicPropertyChangeEvent *>(e);
+	if( ! chev ) break;
+	char const * key = chev->propertyName().constData();
+	this->propertySet( key, this->property(key) );
+	e->accept();
+	break;
+    }
+    return QObject::event(e);
 }
 
 bool QGIHider::serialize( S11nNode & dest ) const
@@ -58,8 +183,23 @@ bool QGIHider::serialize( S11nNode & dest ) const
     {
 	throw s11n::s11n_exception( "QGIHider cannot serialize hidden non-Serializable QGI." );
     }
+
+    if( this->metaObject()->propertyCount() )
+    {
+	QObject props;
+	props.setProperty("zLevel", this->zValue() );
+// 	props.setProperty("angle", this->property("angle") );
+// 	props.setProperty("scale", this->property("scale") );
+	S11nNode & pr( s11n::create_child( dest, "properties" ) );
+	QObject const & constnessKludge( props );
+	if( ! s11n::qt::QObjectProperties_s11n()( pr, constnessKludge ) ) return false;
+    }
+
     return s11n::serialize_subnode( dest, "item", *s )
-	&& s11n::serialize_subnode( dest, "pos", this->pos() );
+	&& s11n::serialize_subnode( dest, "pos", this->pos() )
+	&& s11n::serialize_subnode( dest, "brush", this->brush() )
+        && s11n::serialize_subnode( dest, "transform", this->transform() )
+	;
 }
 
 bool QGIHider::deserialize( S11nNode const & src )
@@ -78,9 +218,10 @@ bool QGIHider::deserialize( S11nNode const & src )
     }
     else
     {
-	qDebug() << "QGIHider::deserialize: couldn't deser 'pos'!";
+	qWarning("QGIHider::deserialize: couldn't deser 'pos'");
     }
-    S11nNode const * ch = s11n::find_child_by_name( src, "item" );
+    S11nNode const * ch = 0;
+    ch = s11n::find_child_by_name( src, "item" );
     if( ! ch ) return true;
     Serializable * s = s11nlite::deserialize<Serializable>( *ch );
     if( ! s )
@@ -96,6 +237,27 @@ bool QGIHider::deserialize( S11nNode const & src )
     pos = this->pos();
     this->hideItem( it );
     this->setPos(pos);
+
+    ch = s11n::find_child_by_name( src, "brush" );
+    if( ch )
+    {
+	QBrush brush;
+	s11n::deserialize( *ch, brush );
+	this->setBrush(brush);
+    }
+    ch = s11n::find_child_by_name( src, "transform" );
+    if( ch )
+    {
+	QTransform tr;
+	s11n::deserialize( *ch, tr );
+	this->setTransform(tr);
+    }
+
+    ch = s11n::find_child_by_name(src, "properties");
+    if( ch )
+    {
+	s11n::qt::QObjectProperties_s11n()( *ch, *this );
+    }
     return true;
 }
 
@@ -152,8 +314,12 @@ void QGIHider::hideItem( QGraphicsItem * toHide )
 	    {
 		color = cv.value<QColor>();
 	    }
+//  	    this->setProperty("angle", obj->property("angle") );
+//  	    this->setProperty("scale", obj->property("scale") );
 	}
-	this->setBrush( QBrush(color, impl->bstyle) );
+	QBrush br( this->brush() );
+	br.setColor(color);
+	this->setBrush( br );
 	if( sc )
 	{
 	    sc->addItem(this);
@@ -263,9 +429,17 @@ void QGIHider::mousePressEvent( QGraphicsSceneMouseEvent * ev )
 
 QRectF QGIHider::boundingRect() const
 {
-    return impl->item
+#if 0
+    QRectF r = impl->item
 	? impl->item->boundingRect()
 	: QRectF(0,0,50,50); // arbitrary
+    return this->transform().mapRect(r);
+#else
+    // This value isn't correct when we're scaled independently of the item.
+    return impl->item
+	? impl->item->boundingRect()
+	: QRectF(0,0,50,50);
+#endif
 }
 QPainterPath QGIHider::shape() const
 {
@@ -300,10 +474,6 @@ void QGIHider::paint(QPainter *painter,
     this->QGraphicsPathItem::paint(painter,opt,wid);
 }
 
-bool QGIHider::event( QEvent * e )
-{
-    return QObject::event(e);
-}
 
 void QGIHider::contextMenuEvent( QGraphicsSceneContextMenuEvent * ev )
 {
@@ -312,6 +482,42 @@ void QGIHider::contextMenuEvent( QGraphicsSceneContextMenuEvent * ev )
     QMenu * m = proxy.createMenu( this );
     m->addAction(QIcon(":/QBoard/icon/box_unwrapped.png"),
 		 "Uncover", this, SLOT(unhideItems()) );
+
+    typedef QList<QObject *> OL;
+    OL selected;
+    if(  this->isSelected() )
+    {
+	selected = qboard::selectedItemsCast<QObject>( this->scene() );
+    }
+    else
+    {
+	selected.push_back( static_cast<QObject*>(this) );
+    }
+
+    if(1)
+    {
+	m->addSeparator();
+	QObjectPropertyMenu * pm =
+	    QObjectPropertyMenu::makeColorMenu(selected, "color","alpha");
+	pm->setIcon(QIcon(":/QBoard/icon/color_fill.png"));
+	m->addMenu(pm);
+
+#if 0
+	// Scaling/rotating independently of the item causes graphics artefacts
+	pm =
+	    QObjectPropertyMenu::makeNumberListMenu("Scale",
+						    selected, "scale",
+						    0.5, 3.01, 0.5);
+	pm->setIcon(QIcon(":/QBoard/icon/viewmag.png"));
+	m->addMenu(pm);
+	m->addMenu( QObjectPropertyMenu::makeNumberListMenu("Rotate",
+							    selected,
+							    "angle",
+							    0,360,15) );
+#endif
+    }
+
+
     m->addSeparator();
     MenuHandlerCopyCut * clipper = new MenuHandlerCopyCut( this, m );
     clipper->addDefaultEntries( m, true, this->isSelected() );
