062f0efc4bfbb0c71128a13122be3a035317a72f
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2008-09-11 22:38:21 +0000

    Added more JS-bound functions.

--- trunk/include/qboard/JSQGI.h
+++ trunk/include/qboard/JSQGI.h
@@ -31,23 +31,65 @@ public:
 
     Q_INVOKABLE QList<QGraphicsItem*> childItems();
 
-Q_SIGNALS:
-    void doubleClicked(QGraphicsItem*);
 
-public Q_SLOTS:
-    QPointF pos(); // why is this not seen script-side?
     /**
        Moves this object to the given coords.
     */
-    void move( qreal x, qreal y );
-    void move( QPointF const & p );
-    qreal posX();
-    qreal posY();
-    //QPointF pos();
-    QScriptValue prop(QString const & key);
-    bool prop(QString const & key, QScriptValue const & val );
+    Q_INVOKABLE void move( qreal x, qreal y );
+    Q_INVOKABLE void moveBy( qreal x, qreal y );
+    /**
+       Moves this object to the given coords.
+    */
+    Q_INVOKABLE void move( QPointF const & p );
 
-    bool setParentItem( QGraphicsItem * );
+    /**
+       Returns the object's current X position, relative to its parent
+       (if it has one) or its scene.
+    */
+    Q_INVOKABLE qreal posX();
+    Q_INVOKABLE qreal posY();
+
+    /**
+       Damn! If this function is called pos() it gets hidden by the pos
+       property of the various game items.
+
+       So it was renamed.
+
+       Returns the current position as a JS object {x:N,y:N,z:N}
+
+       This value can, in turn, be passed to the QPoint or QPointF
+       constructor in JS code, e.g.:
+
+       \code
+       var pos = someItem.posXYZ();
+       pos.x = pos.x + 20;
+       someItem.prop('pos',QPoint(pos));
+       \endcode
+    */
+    Q_INVOKABLE QScriptValue posXYZ();
+
+    /**
+       Returns the value of the given property.
+    */
+    Q_INVOKABLE QScriptValue prop(QString const & key);
+
+    /**
+       Returns the given property to the given value.
+    */
+    Q_INVOKABLE bool prop(QString const & key, QScriptValue const & val );
+
+    /**
+       Sets this object's parent item, transfering ownership of this
+       object to that item.
+
+       Returns false only if this object is not properly bound to
+       an underlying QGraphicsItem.
+    */
+    Q_INVOKABLE bool setParentItem( QGraphicsItem * p );
+
+Q_SIGNALS:
+    void doubleClicked(QGraphicsItem*);
+
 private:
     QGraphicsItem * self();
     struct Impl;
