8724093fb35c9ee3c6c9dc0bf9e9494d9fed7182
sgbeal <sgbeal@bad17cc0-8c4f-0410-af50-c7c4b8b862fa>
2009-05-23 12:51:26 +0000

    Added script to create dist tarball.

---
+++ S11nQt/makeDist.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+########################################################################
+# A quick hack to generate a source dist file for QBoard.
+
+VERSION=$(date +%Y%m%d)
+
+DEST=libS11nQt-1.2.9-${VERSION}
+echo "Creating $DEST ..."
+
+
+test -d ${DEST} && rm -fr ${DEST}
+mkdir ${DEST}
+test -d ${DEST} || {
+    echo "Destination dir [${DEST}] could not be created!"
+    exit 1
+}
+find . -name '*~' | xargs rm -f
+
+sdirs="s11n app include"
+
+echo "Copying files..."
+for d in $sdirs; do
+    echo -e "\t... $d"
+    find $d \
+	-type f \
+	-name '*.h' \
+	-o -name '*.?pp' \
+	-o -name '*.pro' \
+	| sed -e '/\/bak/d' \
+	-e '/\/nono/d' \
+	-e '/\.#/d' \
+	-e '/\.svn/d' \
+	> filelist
+    cp --parents $(cat filelist) $DEST || {
+	err=$?
+	echo "Copy failed!"
+	rm filelist
+	exit $err
+    }
+    rm filelist
+
+    #echo cp --parents $(set -x; find $d $nameargs)
+done
+
+cp \
+    README.txt \
+    LICENSE.GPL? \
+    LICENSE.txt \
+    config.qmake \
+    S11nQt.pro \
+    $DEST
+
+echo "Kludging in static version number..."
+
+echo "Removing a few unwanted files..."
+find $DEST -name 'qrc_*.cpp' | xargs rm -f
+find $DEST -type d -name nono -o -name release -o -name debug | xargs rm -fr
+find $DEST -type d -name  debug -o -name release | xargs rm -fr
+
+echo "Tarring...";
+TF=$DEST.tar.bz2
+tar cjf $TF $DEST
+echo "Cleaning up..."
+rm -fr $DEST
+
+echo "Done:"
+ls -l $TF
